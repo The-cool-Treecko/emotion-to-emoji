111c3047c689744aa7fa037ee14cf4157f25d364
The cool Treecko <77108834+The-cool-Treecko@users.noreply.github.com>
2021-11-08 19:39:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,6 +29,35 @@ function speak() {
     synth = window.speechSynthesis;
     speech_data1 = "The first prediction is " + prediction1;
     speech_data2 = "The second prediction is " + prediction2;
-    utterThis = new SpeechSynthesisUtterance(speech_data1 + speech_data2);
+    utterThis = new SpeechSynthesisUtterance(speech_data1 + " and " + speech_data2);
     synth.speak(utterThis);
+}
+
+function toEmoji() {
+    classifier.classify(document.getElementById("resultimg"), function (error, results) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(results);
+            prediction1 = results[0].label;
+            prediction2 = results[1].label;
+            speak()
+            document.getElementById("result_emotion_name").innerHTML = prediction1
+            document.getElementById("result_emotion_name2").innerHTML = prediction2
+            if (prediction1 == "Angry") {
+                document.getElementById("emoji1").innerHTML = "&#128545"
+            } else if (prediction1 == "Happy") {
+                document.getElementById("emoji1").innerHTML = "&#128512"
+            } else if (prediction1 == "Sad") {
+                document.getElementById("emoji1").innerHTML = "&#128532"
+            }
+            if (prediction2 == "Angry") {
+                document.getElementById("emoji2").innerHTML = "&#128545"
+            } else if (prediction2 == "Happy") {
+                document.getElementById("emoji2").innerHTML = "&#128512"
+            } else if (prediction2 == "Sad") {
+                document.getElementById("emoji2").innerHTML = "&#128532"
+            }
+        }
+    })
 }
